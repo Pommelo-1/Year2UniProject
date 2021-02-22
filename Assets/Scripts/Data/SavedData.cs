6f2664d1788c8eb40f0a653bba65ba930708f607
Hege <19026013@brookes.ac.uk>
2021-02-22 10:08:34 +0000

    Saved data file

---
+++ Assets/Scripts/Data/SavedData.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Data;
+using System;
+using System.Collections.Generic;
+
+/*
+- all the lists they have made
+- prefab lists they have made
+- items in lists
+- settings they changed?
+*/
+
+namespace Assets.Data
+
+{
+    [Serializable]
+    public class SavedData
+    {
+
+        //public List<Habit> habits;
+        //public string themeName;
+
+        public List<UserList> userLists;
+        public List<PrefabList> prefabLists;
+        public List<Items> itemsInList;
+        //settings
+
+        public SavedData(List<UserList> userLists, List<PrefabList> prefabLists, List<Items> itemsInList)
+        {
+            this.userLists = userLists;
+            this.prefabLists = prefabLists;
+            this.itemsInList = itemsInList;
+        }
+    }
+}
